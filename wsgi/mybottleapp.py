a1d53c3cfed8004516b72eb057db3381dc52cba5
Xerrot <xerrot@XerrotDeb>
2014-05-14 21:13:01 +0200

    Purebav2.6

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -58,7 +58,10 @@ def resultado():
     entrada = ''
     numpag = 1
     response.set_cookie('busqueda', str(numpag))
-    return AdNailResultado(appid,numpag,entrada)
+    try:
+        return AdNailResultado(appid,numpag,entrada)
+    except:
+        return template('busqueda_error.html')
 
 @route('/resultado+')
 def resultado():
@@ -67,7 +70,6 @@ def resultado():
     if numpag < 35:
         numpag = numpag + 1
     response.set_cookie('busqueda', str(numpag))
-
     try:
         return AdNailResultado(appid,numpag,entrada)
     except:
