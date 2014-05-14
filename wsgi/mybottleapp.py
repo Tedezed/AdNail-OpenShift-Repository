588c015356f80c5ce8de1a943602a501733938d8
Xerrot <xerrot@XerrotDeb>
2014-05-14 14:37:59 +0200

    unificacion

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import get, post, route, request, run, template, static_file, response
+import commands
+import os
+from bottle import get, post, route, request, run, template, static_file, response, TEMPLATE_PATH, default_app
 from shift_local import shift_local
 from ANResult import AdNailResultado
 
@@ -72,4 +74,19 @@ def resultado():
         return template('busqueda_error.html')
 
 #Deteccion de entorno, OpenShift o local.
-shift_local()
+ON_OPENSHIFT = False
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+    ON_OPENSHIFT = True
+
+if ON_OPENSHIFT:
+    TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'],
+        'runtime/repo/wsgi/views/'))
+    application=default_app()
+else:
+    print "AdNail - Interfaces disponibles: "
+    print commands.getoutput("/sbin/ifconfig | egrep -o '^[a-z].......'")
+    intfz = raw_input('Introduce la interfaz a utilizar: ')
+    comand = "/sbin/ifconfig "+intfz+" | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v '*(0|255)$'"
+    iphost = commands.getoutput(comand)
+    print "La IP del Servidor es: ", iphost
+    run(host=iphost, port=8080, debug=True)
