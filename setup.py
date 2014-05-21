6feff27813179ab145bebc893ff29199948bd85d
Xerrot <xerrot@XerrotDeb>
2014-05-21 14:09:06 +0200

    Nuevo setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Tedezed',
-      version='0.1',
-      description='OpenShift App for bottle',
-      author='Tedezed',
-      author_email='zerrotchanel@gmail.com',
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=[
         'bottle',
